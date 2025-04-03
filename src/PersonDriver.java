9c64324f7bfbeebb50ecd0293e15818fa14b0ac8
Japerz <30984308+Japerz12138@users.noreply.github.com>
2025-04-03 13:08:29 -0400

    Add PersonDirver.java

---
+++ src/PersonDriver.java
@@ -0,0 +1,8 @@
+public class PersonDriver {
+    public static void main(String[] args) {
+        Person p1 = new Person("Bob");
+        Person p2 = new Person("Alice");
+        System.out.println(p1.getName());
+        System.out.println(p2.getName());
+    }
+}
